87402fc491b58f379608f6751503828887256d02
Task_1_1 <cooltone4ka@gmail.com>
2018-11-01 14:01:00 +0200

    /

--- src/main/java/com/tonia/Main.java
+++ src/main/java/com/tonia/Main.java
@@ -1,15 +1,11 @@
 package com.tonia;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String dayOfTheWeek = scanner.nextLine();
-        int hourOfTheDay = scanner.nextInt();
-        boolean isLessonNow =((dayOfTheWeek.equals("Monday") || dayOfTheWeek.equals("Friday")) && (hourOfTheDay >= 18 && hourOfTheDay < 20));
-        if(isLessonNow){
-            System.out.println("Lesson is running");
-        } else{
-            System.out.println("Lesson isn't running");
+        int k = 1;
+        for(int i=2; k <= 20; i=i*2) {
+           k++;
+           System.out.println(i);
         }
 
     }
